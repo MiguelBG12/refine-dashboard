e386f36acece8edd295ee9b2c597ad2d0ece4cf5
MiguelBG12 <miguel.bg12@gmail.com>
2024-08-05 21:11:55 -0500

    Implementation of subscription management and real-time updates

--- enterpriseinsights/src/providers/data/index.tsx
+++ enterpriseinsights/src/providers/data/index.tsx
@@ -1,7 +1,10 @@
-import { GraphQLClient } from "@refinedev/nestjs-query";
+import graphqlDataProvider, { GraphQLClient, liveProvider as graphqlLiveProvider } from "@refinedev/nestjs-query";
+import { createClient } from "graphql-ws";
 import { fetchWrapper } from "./fetch-wrapper";
 
+export const API_BASE_URL = "https://api.crm.define.dev";
 export const API_URL = "https://api.crm.refine.dev";
+export const WS_URL = "wws://api.crm.refine.dev/graphql";
 
 // A GraphQLClient instance is created
 export const client = new GraphQLClient(API_URL, {
@@ -14,3 +17,24 @@ export const client = new GraphQLClient(API_URL, {
     }
   },
 });
+
+// Create the WebSocket client only if you are in a browser environment
+export const wsClient =
+  typeof window !== "undefined"
+    ? createClient({
+        url: WS_URL,
+        connectionParams: () => {
+          const accessToken = localStorage.getItem("access_token");
+
+          return {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          };
+        },
+      })
+    : undefined;
+
+export const dataProvider = graphqlDataProvider(client);
+// Create the liveProvider with the WebSocket client
+export const liveProvider = wsClient ? graphqlLiveProvider(wsClient) : undefined;
