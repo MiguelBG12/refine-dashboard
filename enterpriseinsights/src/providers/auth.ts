224938752a914015548088b36c3af9d15e4e823f
MiguelBG12 <miguel.bg12@gmail.com>
2024-08-08 12:38:23 -0500

    Credentials for demonstration

---
+++ enterpriseinsights/src/providers/auth.ts
@@ -0,0 +1,5 @@
+// For demonstration purposes and to make it easier to test the application, please use the following credentials
+export const authCredentials = {
+  email: "mbautista.g12@gmail.com",
+  password: "123demo",
+};
