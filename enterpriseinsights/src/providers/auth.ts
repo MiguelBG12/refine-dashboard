c21f1c879313fa30629cf196194e502f46e59b4a
MiguelBG12 <miguel.bg12@gmail.com>
2024-08-08 12:57:01 -0500

    An authentication provider is defined that will include several functions for authentication and authorization.

--- enterpriseinsights/src/providers/auth.ts
+++ enterpriseinsights/src/providers/auth.ts
@@ -1,5 +1,10 @@
-// For demonstration purposes and to make it easier to test the application, please use the following credentials
+import { AuthBindings } from "@refinedev/core";
+
+// For demo purposes and to make it easier to test the app, you can use the following credentials
 export const authCredentials = {
   email: "mbautista.g12@gmail.com",
   password: "123demo",
+};
+
+export const authProvider: AuthBindings = {
 };
