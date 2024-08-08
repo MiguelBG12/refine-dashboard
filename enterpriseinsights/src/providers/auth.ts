5daa56cbef121d610a290e069d047c9c900502f4
MiguelBG12 <miguel.bg12@gmail.com>
2024-08-08 12:59:07 -0500

    Login function: Login and save the access token. Handle login errors.

--- enterpriseinsights/src/providers/auth.ts
+++ enterpriseinsights/src/providers/auth.ts
@@ -1,5 +1,7 @@
 import { AuthBindings } from "@refinedev/core";
 
+import { API_URL, dataProvider } from "./data";
+
 // For demo purposes and to make it easier to test the app, you can use the following credentials
 export const authCredentials = {
   email: "mbautista.g12@gmail.com",
@@ -7,4 +9,46 @@ export const authCredentials = {
 };
 
 export const authProvider: AuthBindings = {
+  login: async ({ email }) => {
+    try {
+      // call the login mutation
+      // dataProvider.custom is used to make a custom request to the GraphQL API
+      // this will call dataProvider which will go through the fetchWrapper function
+      const { data } = await dataProvider.custom({
+        url: API_URL,
+        method: "post",
+        headers: {},
+        meta: {
+          variables: { email },
+          // pass the email to see if the user exists and if so, return the accessToken
+          rawQuery: `
+            mutation Login($email: String!) {
+              login(loginInput: { email: $email }) {
+                accessToken
+              }
+            }
+          `,
+        },
+      });
+
+      // save the accessToken in localStorage
+      localStorage.setItem("access_token", data.login.accessToken);
+
+      return {
+        success: true,
+        redirectTo: "/",
+      };
+    } catch (e) {
+      const error = e as Error;
+
+      return {
+        success: false,
+        error: {
+          message: "message" in error ? error.message : "Login failed",
+          name: "name" in error ? error.name : "Invalid email or password",
+        },
+      };
+    }
+  },
+
 };
