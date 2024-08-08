3fedd2bb48a3ebae0e13d2584fe24d715879e690
MiguelBG12 <miguel.bg12@gmail.com>
2024-08-08 13:02:24 -0500

    Check user authentication and redirect.

--- enterpriseinsights/src/providers/auth.ts
+++ enterpriseinsights/src/providers/auth.ts
@@ -74,4 +74,37 @@ export const authProvider: AuthBindings = {
     return { error };
   },
 
+  check: async () => {
+    try {
+      //  get the identity of the user
+      // this is to know if the user is authenticated or not
+      await dataProvider.custom({
+        url: API_URL,
+        method: "post",
+        headers: {},
+        meta: {
+          rawQuery: `
+            query Me {
+              me {
+                name
+              }
+            }
+          `,
+        },
+      });
+
+      // if the user is authenticated, redirect to the home page
+      return {
+        authenticated: true,
+        redirectTo: "/",
+      };
+    } catch (error) {
+      // for any other error, redirect to the login page
+      return {
+        authenticated: false,
+        redirectTo: "/login",
+      };
+    }
+  },
+
 };
